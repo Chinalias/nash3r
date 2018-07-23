f21c91e93d2e64bce24f32ee6b802bd034ec78cd
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-07-23 23:00:16 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,4 +4,4 @@ client.on('ready', () => {
   client.user.setGame(`By | A7MD`,'https://www.twitch.tv/Baktash');
   console.log('BOT ONLINE');
 });
-client.login('MzMxMDgxMjY4NzMxMDUyMDQy.Di5h_Q.Zv7UklLhGjz2f7w9iXOMT1lOSrg');
+client.login('MzMxMDgxMjY4NzMxMDUyMDQy.Dhkqhg.NdUys-v_fw_BEK1nrdbyKc5zAP0');
