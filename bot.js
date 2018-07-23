3075312408aa6b2851df7680fdc094e8b42fd8f3
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-07-23 23:07:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-  client.user.setGame(`By | A7MD`,'https://www.twitch.tv/Baktash');
+  client.user.setGame(`Rlly ?`,'https://www.twitch.tv/Baktash');
   console.log('BOT ONLINE');
 });
 
