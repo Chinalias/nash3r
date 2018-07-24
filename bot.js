d6db11419bf14a5c0ab2235bd39117a2e43348ef
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-07-24 23:42:46 +0200

    Delete bot.js

--- bot.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-client.on('ready', () => {
-  client.user.setGame(`Rlly ?`,'https://www.twitch.tv/Baktash');
-  console.log('BOT ONLINE');
-});
-
-client.login(process.env.BOT_TOKEN);
