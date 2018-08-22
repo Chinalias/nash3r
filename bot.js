e1b23b29c61d48f9a6b9a21090373fefcd3b389f
baktash07 <35742714+baktash07@users.noreply.github.com>
2018-08-22 17:56:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -18,7 +18,7 @@ WELCOME
 •واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
 شرفنا يا عسل:wink: :kissing_heart:
 
-
+**Dark server**
 https://discord.gg/yNvx965 
                      
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
@@ -42,6 +42,7 @@ WELCOME
 •واخيرا حييييياكم واكيد وجودكم هو اللي بيدعمنا وراح نكون الافضل.
 شرفنا يا عسل:wink: :kissing_heart:
 
+**Dark server**
 https://discord.gg/yNvx965
 
 الدعووة خاصة لك ي قلبي ... [ ${member}  ]
